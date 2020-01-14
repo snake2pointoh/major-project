a2a6dbcaa646fa5cd54fa85e61da73287a7c85f5
snake2pointoh <snake2pointoh@users.noreply.github.com>
2020-01-13 22:21:11 -0600

    fixed another bug

--- assets/JS-Files/phys.js
+++ assets/JS-Files/phys.js
@@ -12,15 +12,6 @@ function doPhys() {
 function gamePhys() {
     playerController(Player)
     Player.collisionDetect(mapList[currentMap].grid)
-    if(i === 0){
-        for (let i = 0; i < ai.length; i++) {
-            ai[i].pathfind()
-        }
-        i = 15
-    }
-    else{
-        i--
-    } 
 }
 
 function aiPath() {
